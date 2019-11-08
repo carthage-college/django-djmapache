76ecb483047456e0a46ca664996747c94b657467
plungerman <plungerman@gmail.com>
2019-11-08 14:29:22 -0600

    munge the majors and minors so that they become a json compatible list; also read sql from the files since we do not need any dynamic construction any more

--- djmapache/bin/grover.py
+++ djmapache/bin/grover.py
@@ -4,6 +4,7 @@ import os, sys
 import csv
 import argparse
 import logging
+import json
 
 # env
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmapache.settings.shell')
@@ -49,8 +50,8 @@ HEADERS['alumni'] = HEADERS['student'] = HEADERS['facstaff'] + [
     'Social Class Year','Graduation Year Admin Only'
 ]
 HEADERS['education'] = [
-    'User Type','Email Address','Database Key','Graduation Year','School',
-    'Degree Type','Majors','Minors'
+    'Database Key','Email Address','School Name','School Degree',
+    'School Major(s)','School Minor','School Year'
 ]
 
 
@@ -63,7 +64,8 @@ def main():
         print("who must be: 'student', 'facstaff', 'alumni', or 'education'\n")
         exit(-1)
 
-    phile = '{}/sql/grover/{}.sql'.format(settings.BASE_DIR,who)
+    #phile = '{}/sql/grover/{}.sql'.format(settings.BASE_DIR,who)
+    phile = os.path.join(settings.BASE_DIR, 'sql/grover', '{}.sql'.format(who))
     with open(phile) as incantation:
         sql = incantation.read()
 
@@ -81,11 +83,23 @@ def main():
 
     phile = r'{}.csv'.format(who)
     with open(phile, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter='|')
+        writer = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_NONE, quotechar='')
         writer.writerow([x for x in headers])
         char_remove = set([' ','(',')'])
         for row in rows:
-            row.email = ''.join([c for c in row.email if c not in char_remove])
+            # sometimes the provisioning view will include an entity that was
+            # just created and might not have a username just yet.
+            if row.email:
+                row.email = ''.join([c for c in row.email if c not in char_remove])
+            # grover's import app does not like trailing commas in the list.
+            # python 3 returns an iterator from filter(), so we wrap it in list()
+            # grover also wants double quotes and not single quotes so we
+            # convert the list to json string with dumps()
+            majors = list(filter(None, row.majors.split(',')))
+            row.majors = json.dumps(majors)
+            minors = list(filter(None, row.minors.split(',')))
+            row.minors = json.dumps(minors)
+            # write the row
             writer.writerow(row)
 
     print('done. created file: {}'.format(phile))
