090d792c9a9b815c51135d274a8f5e5211d59563
dsullivan <dsullivan@carthage.edu>
2020-02-07 14:36:08 -0600

    moved functions to utilities file, now writing reformatted file

---
+++ djmapache/workday/hcm_hr/utilities.py
@@ -0,0 +1,54 @@
+import csv
+
+def fn_format_country(cntry_code):
+    if cntry_code == "USA":
+        cntry = 'United States of America'
+    elif cntry_code == "CAN":
+        cntry = 'Canada'
+    elif cntry_code == "CHN":
+        cntry = 'China'
+    elif cntry_code == "DEU":
+        cntry = 'German'
+    elif cntry_code == "SRB":
+        cntry = 'Serbia'
+    else:
+        cntry = "Country Unavailable"
+
+    return cntry
+
+def fn_format_phone(cntry_code, phone):
+    if cntry_code == "USA":
+        intlcode = '1'
+        area = phone[1:4]
+        phon = phone[6:14]
+    elif cntry_code == "CAN":
+        intlcode = '1'
+        area = phone[1:4]
+        phon = phone[6:14]
+    # elif cntry_code == "CHN":
+    #     cntry = 'China'
+    # elif cntry_code == "DEU":
+    #     cntry = 'German'
+    # elif cntry_code == "SRB":
+    #     cntry = 'Serbia'
+    else:
+        intlcode = '1'
+        area = phone[1:4]
+        phon = phone[6:14]
+
+    return intlcode, area, phon
+
+def fn_write_phone_cl_header(file):
+    print(file)
+    with open(file, 'w') as phone_output:
+        csvwriter = csv.writer(phone_output)
+        csvwriter.writerow(["Worker ID", "Phone Type", "Country (Phone)",
+                            "International Phone Code", "Area Code",
+                            "Phone Number", "Phone Extension", "Public"])
+
+
+def fn_write_phone_cl(file, data):
+    print(file)
+    with open(file, 'a') as phone_output:
+        csvwriter = csv.writer(phone_output)
+        csvwriter.writerow(data)
