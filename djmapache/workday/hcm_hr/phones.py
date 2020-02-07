6ac64d1a48175072a13764320ace6d3f9ca688c3
dsullivan <dsullivan@carthage.edu>
2020-02-07 14:35:10 -0600

    moved functions to utilities file, now writing reformatted file

--- djmapache/workday/hcm_hr/phones.py
+++ djmapache/workday/hcm_hr/phones.py
@@ -2,7 +2,6 @@ import os
 import sys
 # import pysftp
 import csv
-import codecs
 import argparse
 import logging
 from logging.handlers import SMTPHandler
@@ -17,10 +16,9 @@ django.setup()
 # django settings for script
 from django.conf import settings
 from djimix.core.utils import get_connection, xsql
-# from djlabour.sql.phone_sql import CX_VIEW_SQL, Q_phone_VERIFY, \
-#     INS_phone_REC
-# from djlabour.core.phone_utilities import fn_write_adp_header, \
-#     fn_write_header, fn_write_row_reformatted, fn_write_error, fn_send_mail
+from djmapache.workday.hcm_hr.utilities import fn_format_country, \
+    fn_format_phone, fn_write_phone_cl_header, fn_write_phone_cl
+
 
 # informix environment
 os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
@@ -107,6 +105,7 @@ parser.add_argument(
 #     sftp.close()
 
 
+
 def main():
 
     ##########################################################################
@@ -116,57 +115,42 @@ def main():
 
     # # Defines file names and directory location
     # if test:
-    phone_csv_output = "/home/dsullivan/djlmapache/djlmapache/workday/hcm_hr/cleandata/"
+    phone_csv_output = "/home/dsullivan/djmapache/djmapache/workday/hcm_hr/clean_data/"
     phone_csv_input = "/home/dsullivan/djmapache/djmapache/workday/hcm_hr/raw_data/"
     # else:
     # phone_csv_output = settings.phone_csv_output
     # print(phone_csv_output)
-
 
     # For testing use last file
     # new_phone_file = phone_csv_output + "ADPtoCXLast.csv"
     raw_phone_file = phone_csv_input + "phones.csv"
     new_phone_file = phone_csv_output + "phones_cln.csv"
 
-    # First remove yesterdays file of updates
-
     try:
-        # # set global variable
-        # global EARL
-        # # determines which database is being called from the command line
-        # if database == 'cars':
-        #     EARL = settings.INFORMIX_ODBC
-        # if database == 'train':
-        #     EARL = settings.INFORMIX_ODBC_TRAIN
-        # else:
-        #     # # this will raise an error when we call get_engine()
-        #     # below but the argument parser should have taken
-        #     # care of this scenario and we will never arrive here.
-        #     EARL = None
-        #     # establish database connection
-        # # print(EARL)
+        # set global variable
+        global EARL
+        # determines which database is being called from the command line
+        if database == 'cars':
+            EARL = settings.INFORMIX_ODBC
+        if database == 'train':
+            EARL = settings.INFORMIX_ODBC_TRAIN
+        else:
+            # # this will raise an error when we call get_engine()
+            # below but the argument parser should have taken
+            # care of this scenario and we will never arrive here.
+            EARL = None
+            # establish database connection
+        print(EARL)
 
-        #################################################################
-        # STEP 0--
         # Pull the file from the ADP FTP site
         # execute sftp code in production only
-        #################################################################
         # if not test:
         #     file_download()
 
-        #################################################################
-        # STEP 1--
         # Create the new file for the formatted data
-        #################################################################
-        # fn_write_phone_cl_header(adptocx_reformatted)
-        # Header for new file = "Worker ID", "Phone Type", "Country (Phone)",
-        # "International Phone Code", "Area Code", "Phone Number",
-        # "Phone Extension", "Public"
+        fn_write_phone_cl_header(new_phone_file)
 
-        #################################################################
-        # STEP 2--
         # Read the raw file and work on the formatting
-        #################################################################
         # print(raw_phone_file)
 
         with open(raw_phone_file, 'r') as f:
@@ -174,6 +158,8 @@ def main():
             for row in d_reader:
                 # print("+++++++++++++++++++++")
                 # print(row)
+
+                print_flag = False
 
                 """Student workers and a few others typically don't have the
                     Carthage ID Custom field populated  May have to force it
@@ -183,6 +169,7 @@ def main():
                     if row['File Number'] == "":
                         print("No id or file number")
                         pass
+
                     else:
                         workerid = "1" + row['File Number']
                 else:
@@ -190,68 +177,52 @@ def main():
 
                 """Format country and phone - I don't know if our ADP  data 
                     has any phone numbers that aren't in the US format"""
+                cntry = fn_format_country(row['Primary Address: Country Code'])
 
-                if row['Primary Address: Country Code'] == "USA":
-                    cntry = 'United States of America'
-                    intlcode = "1"
-                elif row['Primary Address: Country Code'] == "CAN":
-                    cntry = 'Canada'
-                    intlcode = "1"
-                elif row['Primary Address: Country Code'] == "CHN":
-                    cntry = 'China'
-                elif row['Primary Address: Country Code'] == "DEU":
-                    cntry = 'German'
-                elif row['Primary Address: Country Code'] == "SRB":
-                    cntry = 'Serbia'
-                else:
-                    cntry = ""
-                    area = ""
-                    phon = ""
-                    print(workerid + " Country unclear")
+                if len(row['Personal Contact: Home Phone']) != 0:
+                    ret = fn_format_phone(row['Primary Address: Country Code'],
+                                          row['Personal Contact: Home Phone'])
+                    intlcode = ret[0]
+                    area = ret[1]
+                    phon = ret[2]
+                    phon_typ = "Home Phone"
+                    fn_write_phone_cl(new_phone_file, [workerid + ','
+                        + phon_typ + ',' + cntry + ',' + intlcode + ','
+                        + area + ',' + phon + ',' + '' + ',' + 'Public'])
 
+                if len(row['Personal Contact: Personal Mobile']) != 0:
+                    ret = fn_format_phone(row['Primary Address: Country Code'],
+                                       row['Personal Contact: Personal Mobile'])
+                    intlcode = ret[0]
+                    area = ret[1]
+                    phon = ret[2]
+                    phon_typ = "Personal Mobile"
+                    fn_write_phone_cl(new_phone_file, [workerid + ','
+                        + phon_typ + ',' + cntry + ',' + intlcode + ','
+                        + area + ',' + phon + ',' + '' + ',' + 'Public'])
 
-                if row['Personal Contact: Home Phone'] != "":
-                    area = row['Personal Contact: Home Phone'][1:4]
-                    phon = row['Personal Contact: Home Phone'][6:14]
-                    print(workerid + ',' + "Home" + ','
-                          + cntry + ',' + intlcode + ',' + area + ',' + phon
-                          + ',' + 'Phone Extension' + ',' + 'Public')
+                if len(row['Work Contact: Work Phone']) != 0:
+                    ret = fn_format_phone(row['Primary Address: Country Code'],
+                                          row['Work Contact: Work Phone'])
+                    intlcode = ret[0]
+                    area = ret[1]
+                    phon = ret[2]
+                    phon_typ = "Work Office"
+                    fn_write_phone_cl(new_phone_file, [workerid + ','
+                        + phon_typ + ',' + cntry + ',' + intlcode + ','
+                        + area + ',' + phon + ',' + '' + ',' + 'Public'])
 
-                if row['Personal Contact: Personal Mobile'] != "":
-                    area = row['Personal Contact: Personal Mobile'][1:4]
-                    phon = row['Personal Contact: Personal Mobile'][6:14]
-                    print(workerid + ','
-                          + "Personal Mobile" + ','
-                          + cntry + ',' + intlcode + ',' + area + ',' + phon
-                          + ',' + 'Phone Extension' + ',' + 'Public')
-
-                if row['Work Contact: Work Phone'] != "":
-                    area = row['Work Contact: Work Phone'][1:4]
-                    phon = row['Work Contact: Work Phone'][6:14]
-                    print(workerid + ','
-                          + "Work Office" + ','
-                          + cntry + ',' + intlcode + ',' + area + ',' + phon
-                          + ',' + 'Phone Extension' + ',' + 'Public')
-
-                if row['Work Contact: Work Mobile'] != "":
-                    area = row['Work Contact: Work Mobile'][1:4]
-                    phon = row['Work Contact: Work Mobile'][6:14]
-                    print(workerid + ','
-                          + "Work Mobile" + ','
-                          + cntry + ',' + intlcode + ',' + area + ',' + phon
-                          + ',' + 'Phone Extension' + ',' + 'Public')
-
-
-            # except Exception as e:
-            #     # print(repr(e))
-            #     fn_write_error("Error in phone_rec.py Step 4, Error = "
-            #                    + repr(e))
-            #     fn_send_mail(settings.ADP_TO_EMAIL, settings.ADP_FROM_EMAIL,
-            #              "Error in phone_rec.py, at reading diff file.  "
-            #              "Error = " + repr(e),
-            #              "Error in phone_rec.py")
-
-        f.close()
+                if len(row['Work Contact: Work Mobile']) != 0:
+                    ret = fn_format_phone(
+                        row['Primary Address: Country Code'],
+                        row['Personal Contact: Work Mobile'])
+                    intlcode = ret[0]
+                    area = ret[1]
+                    phon = ret[2]
+                    phon_typ = "Work Mobile"
+                    fn_write_phone_cl(new_phone_file, [workerid + ','
+                        + phon_typ + ',' + cntry + ',' + intlcode + ','
+                        + area + ',' + phon + ',' + '' + ',' + 'Public'])
 
     except Exception as e:
         print("Error in phone_rec.py, Error = " + repr(e))
@@ -263,18 +234,18 @@ def main():
 
 if __name__ == "__main__":
     main()
-#     args = parser.parse_args()
-#     test = args.test
-#     database = args.database
-#
-# if not database:
-#     print("mandatory option missing: database name\n")
-#     exit(-1)
-# else:
-#     database = database.lower()
-#
-# if database != 'cars' and database != 'train' and database != 'sandbox':
-#     print("database must be: 'cars' or 'train' or 'sandbox'\n")
-#     parser.print_help()
+    args = parser.parse_args()
+    test = args.test
+    database = args.database
 
-sys.exit(main())
+    if not database:
+        print("mandatory option missing: database name\n")
+        exit(-1)
+    else:
+        database = database.lower()
+
+    if database != 'cars' and database != 'train' and database != 'sandbox':
+        print("database must be: 'cars' or 'train' or 'sandbox'\n")
+        parser.print_help()
+
+    sys.exit(main())
