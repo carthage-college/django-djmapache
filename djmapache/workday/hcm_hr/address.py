71d851f1fe19ded27fa6db3f45dc1afe5f618f27
dsullivan <dsullivan@carthage.edu>
2020-02-13 16:22:46 -0600

    working to modularize and systematize all the workday scripts

--- djmapache/workday/hcm_hr/address.py
+++ djmapache/workday/hcm_hr/address.py
@@ -17,7 +17,7 @@ from openpyxl import Workbook
 from django.conf import settings
 from djimix.core.utils import get_connection, xsql
 from djmapache.workday.hcm_hr.utilities import fn_format_country, \
-    fn_format_phone, fn_write_phone_cl_header, fn_write_phone_cl
+    fn_write_addr_cl_header, fn_write_addr_cl, fn_get_id
 
 
 # informix environment
@@ -94,51 +94,27 @@ parser.add_argument(
 #                 #############################################################
 #                 # sftp.remove(filename)
 #         except Exception as e:
-#             # print("Error in phone_rec.py- File download, " + e.message)
-#             fn_write_error("Error in phone_rec.py - File download, "
+#             # print("Error in address.py- File download, " + e.message)
+#             fn_write_error("Error in address.py - File download, "
 #                            "adptocx.csv not found, " +  repr(e))
 #             fn_send_mail(settings.ADP_TO_EMAIL, settings.ADP_FROM_EMAIL,
-#                 "Error in phone_rec.py - File download, "
+#                 "Error in address.py - File download, "
 #                 "adptocx.csv not found," + repr(e),
-#                 "Error in phone_rec.py - File download")
+#                 "Error in address.py - File download")
 #
 #     sftp.close()
-
-def fn_get_id(adp_id, EARL):
-    try:
-        connection = get_connection(EARL)
-        # connection closes when exiting the 'with' block
-        QUERY = '''select cx_id_char
-            from cvid_rec where adp_id = {0}'''.format(adp_id)
-
-        with connection:
-            data_result = xsql(
-                QUERY, connection, key=settings.INFORMIX_DEBUG
-            ).fetchone()
-            return data_result[0].strip()
-            # print(data_result[0])
-            # ret = data_result[0]
-            # print("Carthage id is: " + ret)
-
-    except Exception as e:
-        print("Error in phone_rec.py fn_get_id , Error = " + repr(e))
-        # fn_write_error("Error in phone_rec.py - fn_get_id: "
-        #                + repr(e))
-        # fn_send_mail(settings.ADP_TO_EMAIL, settings.ADP_FROM_EMAIL,
-        #          "Error in phone_rec.py fn_get_id, Error = " + repr(e),
-        #          "Error in phone_rec.py fn_get_id")
 
 
 def fn_clear_sheet(email_csv_output, new_xl_file):
     wb_obj = openpyxl.load_workbook(email_csv_output + new_xl_file)
-    this_sheet = wb_obj['Phone']
+    this_sheet = wb_obj['Address']
     print(this_sheet)
     row_count = this_sheet.max_row
     col_count = this_sheet.max_column
     print(row_count)
     print(col_count)
 
-    for row in this_sheet['A3:H' + str(row_count)]:
+    for row in this_sheet['A3:N' + str(row_count)]:
         # print(row)
         for cell in row:
             # print(cell.value)
@@ -147,9 +123,10 @@ def fn_clear_sheet(email_csv_output, new_xl_file):
 
     wb_obj.save(email_csv_output + new_xl_file)
 
-def fn_insert_xl(ct, workerid, phon_typ, cntry, intlcode, area, phon, public,
+def fn_insert_xl(ct, workerid, cntry, addr_typ, addr_use, region, subregion,
+                 city, city_subdv, postal_cod, addr1, addr2, addr3, addr4,
+                 rem_ee,
                  csv_output, new_xl_file):
-
     try:
         wb_obj = openpyxl.load_workbook(csv_output
                                         + new_xl_file)
@@ -161,25 +138,33 @@ def fn_insert_xl(ct, workerid, phon_typ, cntry, intlcode, area, phon, public,
         # it when the work is done.
 
         # # print(wb_obj.sheetnames)
-        this_sheet = wb_obj['Phone']
+        this_sheet = wb_obj['Address']
         # print(this_sheet)
         this_sheet.cell(row=ct, column=1).value = workerid
-        this_sheet.cell(row=ct, column=2).value = phon_typ
-        this_sheet.cell(row=ct, column=3).value = cntry
-        this_sheet.cell(row=ct, column=4).value = intlcode
-        this_sheet.cell(row=ct, column=5).value = area
-        this_sheet.cell(row=ct, column=6).value = phon
-        this_sheet.cell(row=ct, column=8).value = 'Public'
+        this_sheet.cell(row=ct, column=2).value = cntry
+        this_sheet.cell(row=ct, column=3).value = addr_typ
+        this_sheet.cell(row=ct, column=4).value = addr_use
+        this_sheet.cell(row=ct, column=5).value = region
+        this_sheet.cell(row=ct, column=6).value = subregion
+        this_sheet.cell(row=ct, column=7).value = city
+
+        this_sheet.cell(row=ct, column=8).value = city_subdv
+        this_sheet.cell(row=ct, column=9).value = postal_cod
+        this_sheet.cell(row=ct, column=10).value = addr1
+        this_sheet.cell(row=ct, column=11).value = addr2
+        this_sheet.cell(row=ct, column=12).value = addr3
+        this_sheet.cell(row=ct, column=13).value = addr4
+        this_sheet.cell(row=ct, column=14).value = rem_ee
 
         wb_obj.save(csv_output + new_xl_file)
 
     except Exception as e:
-        print("Error in phone_rec.py fn_ins_xl , Error = " + repr(e))
-    # fn_write_error("Error in phone_rec.py - fn_get_id: "
+        print("Error in address.py fn_ins_xl , Error = " + repr(e))
+    # fn_write_error("Error in address.py - fn_get_id: "
     #                + repr(e))
     # fn_send_mail(settings.ADP_TO_EMAIL, settings.ADP_FROM_EMAIL,
-    #          "Error in phone_rec.py fn_get_id, Error = " + repr(e),
-    #          "Error in phone_rec.py fn_get_id")
+    #          "Error in address.py fn_get_id, Error = " + repr(e),
+    #          "Error in address.py fn_get_id")
 
 def main():
 
@@ -204,7 +189,7 @@ def main():
     new_file = csv_output + "address_cln.csv"
     new_xl_file = "Worker Data.xlsx"
 
-    # fn_clear_sheet(email_csv_output, new_xl_file)
+    fn_clear_sheet(csv_output, new_xl_file)
 
     try:
         # set global variable
@@ -228,7 +213,7 @@ def main():
         #     file_download()
 
         # Create the new csv file for the formatted data
-        # fn_write_phone_cl_header(new_file)
+        fn_write_addr_cl_header(new_file)
 
         # Read the raw file and work on the formatting
         # print(raw_file)
@@ -239,8 +224,8 @@ def main():
             # Write to Excel spreadsheet
 
             for row in d_reader:
-                print("+++++++++++++++++++++")
-                print(row)
+                # print("+++++++++++++++++++++")
+                # print(row)
                 r = r+1
                 # print("ROW " + str(r))
                 # print("Inserts " + str(ct))
@@ -257,87 +242,58 @@ def main():
                         pass
                     else:
                         # print(str(row['File Number']))
-                        workerid = fn_get_id(str(row['File Number']), EARL)
-                        print(workerid)
+                        workerid = fn_get_id(str(row['File Number']), EARL,
+                                            settings.INFORMIX_DEBUG)
+                        # print(workerid)
                 else:
                     workerid = row['Carthage ID #']
-                    print(workerid)
+                    # print(workerid)
 
                 """Format country and phone - I don't know if our 
                 ADP  data
                     has any phone numbers that aren't in the US 
                     format"""
 
-                # cntry = fn_format_country(row['Primary Address: Country Code'])
+                cntry = fn_format_country(row['Primary Address: Country Code'])
+                # cntry = row['Primary Address: Country Code']
+                addr_typ = "Primary Home"
+                addr_use = "Other - Home"
+                region = row["Primary Address: State / Territory Code"]
+                subregion = ""
+                city = row["Primary Address: City"]
+                city_subdv = ""
+                postal_cod = row["Primary Address: Zip / Postal Code"]
+                addr1 = row["Primary Address: Address Line 1"]
+                addr2 = row["Primary Address: Address Line 2"]
+                addr3 = ""
+                addr4 = ""
 
-                # if len(row['Personal Contact: Home Phone']) != 0:
-                #     ret = fn_format_phone(row['Primary Address: Country Code'],
-                #                           row['Personal Contact: Home Phone'])
-                #     intlcode = ret[0]
-                #     area = ret[1]
-                #     phon = ret[2]
-                #     phon_typ = "Home Phone"
-                #     fn_write_phone_cl(new_file, [workerid + ','
-                #         + phon_typ + ',' + cntry + ',' + intlcode
-                #         + ','
-                #         + area + ',' + phon + ',' + '' + ',' + 'No'])
-                #     ct = ct + 1
-                #     fn_insert_xl(ct, workerid, phon_typ, cntry, intlcode, area,
-                #                  phon, 'public', csv_output, new_xl_file)
-                #
-                # if len(row['Personal Contact: Personal Mobile']) != 0:
-                #     ret = fn_format_phone(row['Primary Address: Country Code'],
-                #                    row['Personal Contact: Personal Mobile'])
-                #     intlcode = ret[0]
-                #     area = ret[1]
-                #     phon = ret[2]
-                #     phon_typ = "Personal Mobile"
-                #     fn_write_phone_cl(new_file, [workerid + ','
-                #         + phon_typ + ',' + cntry + ',' + intlcode
-                #         + ','
-                #         + area + ',' + phon + ',' + '' + ',' + 'No'])
-                #     ct = ct + 1
-                #     fn_insert_xl(ct, workerid, phon_typ, cntry, intlcode, area,
-                #                  phon, 'public', csv_output, new_xl_file)
-                #
-                # if len(row['Work Contact: Work Phone']) != 0:
-                #     ret = fn_format_phone(row['Primary Address: Country Code'],
-                #                           row['Work Contact: Work Phone'])
-                #     intlcode = ret[0]
-                #     area = ret[1]
-                #     phon = ret[2]
-                #     phon_typ = "Work Office"
-                #     fn_write_phone_cl(new_file, [workerid + ','
-                #         + phon_typ + ',' + cntry + ',' + intlcode
-                #         + ','
-                #         + area + ',' + phon + ',' + '' + ',' + 'No'])
-                #     ct = ct + 1
-                #     fn_insert_xl(ct, workerid, phon_typ, cntry, intlcode, area,
-                #                  phon, 'public', csv_output, new_xl_file)
-                #
-                # if len(row['Work Contact: Work Mobile']) != 0:
-                #     ret = fn_format_phone(
-                #         row['Primary Address: Country Code'],
-                #         row['Personal Contact: Work Mobile'])
-                #     intlcode = ret[0]
-                #     area = ret[1]
-                #     phon = ret[2]
-                #     phon_typ = "Work Mobile"
-                #     fn_write_phone_cl(new_file, [workerid + ','
-                #         + phon_typ + ',' + cntry + ',' + intlcode
-                #         + ','
-                #         + area + ',' + phon + ',' + '' + ',' + 'No'])
-                #     ct = ct + 1
-                #     fn_insert_xl(ct, workerid, phon_typ, cntry, intlcode,
-                #         area, phon, 'public', csv_output, new_xl_file)
+                # Not sure how to populate this...
+                remote_EE = ""
+
+                # print(workerid, cntry, addr_typ, addr_use, region, city,
+                #       postal_cod, addr1, addr2)
+
+                if row["Primary Address: Country Code"] == 'USA':
+                    fn_write_addr_cl(new_file, [workerid + ',' + cntry + ','
+                        + addr_typ + ',' + addr_use + ',' + region + ','
+                        + subregion + ',' + city + ',' + city_subdv + ','
+                        + postal_cod + ',' + addr1 + ',' + addr2 + ','
+                        + addr3 + ',' + addr4 + ',' + remote_EE])
+                    ct = ct + 1
+
+                    fn_insert_xl(ct, workerid, cntry, addr_typ, addr_use,
+                        region, subregion, city, city_subdv, postal_cod,
+                        addr1, addr2, addr3, addr4,  remote_EE,
+                        csv_output, new_xl_file)
 
     except Exception as e:
-        print("Error in phone_rec.py, Error = " + repr(e))
-        # fn_write_error("Error in phone_rec.py - Main: "
+        print("Error in address.py, Error = " + repr(e))
+        # fn_write_error("Error in address.py - Main: "
         #                + repr(e))
         # fn_send_mail(settings.ADP_TO_EMAIL, settings.ADP_FROM_EMAIL,
-        #          "Error in phone_rec.py, Error = " + repr(e),
-        #          "Error in phone_rec.py")
+        #          "Error in address.py, Error = " + repr(e),
+        #          "Error in address.py")
 
 if __name__ == "__main__":
     args = parser.parse_args()
