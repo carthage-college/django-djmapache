46e1cd1ef59aae9a498a3a7ac9ffa3813090dbdd
plungerman <plungerman@gmail.com>
2020-11-30 11:54:06 -0600

    code clean up

--- djmapache/bin/grover.py
+++ djmapache/bin/grover.py
@@ -6,6 +6,7 @@ import django
 import json
 import logging
 import os
+
 import sys
 
 
@@ -21,91 +22,108 @@ logger = logging.getLogger('djmapache')
 
 # set up command-line options
 desc = """
-    Accepts as input a group type: student, facstaff, alumni, or educatdion
+    Accepts as input a group type: student, facstaff, alumni, or education
 """
 
 parser = argparse.ArgumentParser(
-    description=desc, formatter_class=argparse.RawTextHelpFormatter
+    description=desc, formatter_class=argparse.RawTextHelpFormatter,
 )
 parser.add_argument(
-    '-w', '--who',
+    '--who',
     required=True,
     help="student, facstaff, alumni, or education",
-    dest='who'
+    dest='who',
 )
 parser.add_argument(
     '--pseudo',
     action='store_true',
     help="Include pseudo alumni?",
-    dest='pseudo'
+    dest='pseudo',
 )
 parser.add_argument(
     '--test',
     action='store_true',
     help="Dry run?",
-    dest='test'
+    dest='test',
 )
 
 HEADERS = {}
 HEADERS['facstaff'] = [
-    'User Type','Email Address','Database Key','Last Name','First Name',
-    'Preferred Name','Previous Last Name'
+    'User Type',
+    'Email Address',
+    'Database Key',
+    'Last Name',
+    'First Name',
+    'Preferred Name',
+    'Previous Last Name',
 ]
-HEADERS['alumni'] = HEADERS['student'] = HEADERS['facstaff'] + [
-    'Transcript First Name','Transcript Last Name',
-    'Concentration','Majors Admin Only','Minors Admin Only',
-    'Social Class Year','Graduation Year'
+HEADERS['alumni'] = HEADERS['facstaff'] + [
+    'Transcript First Name',
+    'Transcript Last Name',
+    'Concentration',
+    'Majors Admin Only',
+    'Minors Admin Only',
+    'Social Class Year',
+    'Graduation Year',
 ]
+HEADERS['student'] = HEADERS['alumni']
 HEADERS['education'] = [
-    'Database Key','Email Address','School Name','School Degree',
-    'School Major(s)','School Minor','School Year'
+    'Database Key',
+    'Email Address',
+    'School Name',
+    'School Degree',
+    'School Major(s)',
+    'School Minor',
+    'School Year',
 ]
 
 
 def main():
-
+    """Export data for various constituent types."""
     # check for profile type will fail if not one of the four allowed types
     try:
         headers = HEADERS[who]
-    except:
+    except Exception:
         print("who must be: 'student', 'facstaff', 'alumni', or 'education'\n")
-        print("who = {}".format(who))
-        exit(-1)
+        print("who = {0}".format(who))
+        sys.exit(-1)
 
     suffix = ''
     if pseudo:
         suffix = '_pseudo'
 
-    phile = os.path.join(settings.BASE_DIR, 'sql/grover', '{}{}.sql'.format(
-        who, suffix
+    phile = os.path.join(settings.BASE_DIR, 'sql/grover', '{0}{1}.sql'.format(
+        who, suffix,
     ))
     with open(phile) as incantation:
         sql = incantation.read()
 
     if test:
-        print("who = {}".format(who))
+        print("who = {0}".format(who))
         print("headers")
         print(headers)
         print("phile:")
         print(phile)
-        print("sql = {}".format(sql))
-        logger.debug("sql = {}".format(sql))
-        exit(-1)
+        print("sql = {0}".format(sql))
+        logger.debug("sql = %s", sql)
+        sys.exit(-1)
 
     connection = get_connection()
     with connection:
         rows = xsql(sql, connection, key=settings.INFORMIX_DEBUG).fetchall()
 
-    phile = r'{}{}.csv'.format(who, suffix)
+    phile = r'{0}{1}.csv'.format(who, suffix)
     with open(phile, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_NONE, quotechar='')
-        writer.writerow([x for x in headers])
-        char_remove = set([' ','(',')'])
+        writer.writerow([head for head in headers])
+        char_remove = set([' ', '(', ')'])
         for row in rows:
             # sometimes the provisioning view will include an entity that was
             # just created and might not have a username just yet.
             if row.email:
-                row.email = ''.join([c for c in row.email if c not in char_remove])
+                row.email = ''.join(
+                    [char for char in row.email if char not in char_remove],
+                )
             # grover's import app does not like trailing commas in the list.
             # python 3 returns an iterator from filter(), so we wrap it in list()
             # grover also wants double quotes and not single quotes so we
@@ -121,7 +139,7 @@ def main():
             # write the row
             writer.writerow(row)
 
-    print('done. created file: {}'.format(phile))
+    print('done. created file: {0}'.format(phile))
 
 
 if __name__ == '__main__':
